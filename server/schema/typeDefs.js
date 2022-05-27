506f191723044dfee7bd29830914dda1d4a5836b
Chloe Yarborough <cmyarboro@gmail.com>
2022-05-26 22:18:17 -0400

    added typeDefs

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,51 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create out typeDefs
+const typeDefs = gql`
+type Book {
+    _id: ID
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+input saveBook {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    savedBook(input: saveBook): User
+    removeBook(bookId: String!): User
+}
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
