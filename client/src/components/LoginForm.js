dc43df0a9c8443207022e66b32f5d1487de60fae
Chloe Yarborough <cmyarboro@gmail.com>
2022-05-27 23:32:11 -0400

    updated login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,9 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -10,8 +12,11 @@ const LoginForm = () => {
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
+  const [loginUser] = useMutation(LOGIN_USER);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
+    
     setUserFormData({ ...userFormData, [name]: value });
   };
 
@@ -26,17 +31,13 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await loginUser({
+        variables: {...userFormData}
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
+      Auth.login(data.loginUser.token);
+    } catch (e) {
+      console.error(e);
       setShowAlert(true);
     }
 
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -26,7 +26,7 @@ const SavedBooks = () => {
 
     try {
       await removeBook ({
-      variables: {bookId: bookId}
+      variables: { bookId: bookId }
     });
 
       // upon success, remove book's id from localStorage
